baa2315a8d9aa1184f8662901a7a6da05dcf03e4
Sparsh Bansal <sparsh.bansal17895@gmail.com>
2017-04-02 12:31:14 +0530

    Improved UI

--- app/src/main/java/com/awesomedev/smartindiahackathon/Activities/DetailsActivity.java
+++ app/src/main/java/com/awesomedev/smartindiahackathon/Activities/DetailsActivity.java
@@ -49,6 +49,7 @@ public class DetailsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_details);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
