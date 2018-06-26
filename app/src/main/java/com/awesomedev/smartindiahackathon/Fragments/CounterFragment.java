726e6795d4ebbd99ee9a44528543a6ae49e75461
Sparsh Bansal <sparsh.bansal17895@gmail.com>
2018-06-26 11:00:00 +0530

    Changed counter screen

--- app/src/main/java/com/awesomedev/smartindiahackathon/Fragments/CounterFragment.java
+++ app/src/main/java/com/awesomedev/smartindiahackathon/Fragments/CounterFragment.java
@@ -12,8 +12,10 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.awesomedev.smartindiahackathon.CustomViews.CounterConstraintView;
 import com.awesomedev.smartindiahackathon.CustomViews.CounterViews;
 import com.awesomedev.smartindiahackathon.Data.DatabaseContract;
 import com.awesomedev.smartindiahackathon.Models.Counter;
@@ -35,12 +37,17 @@ import butterknife.ButterKnife;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class CounterFragment extends Fragment implements LoaderManager.LoaderCallbacks<Cursor>{
+public class CounterFragment extends Fragment implements LoaderManager.LoaderCallbacks<Cursor> {
 
 
     private static final String TAG = CounterFragment.class.getSimpleName();
 
-    CounterViews mView = null;
+
+    @BindView(R.id.ll_counter_views)
+    LinearLayout llCounterViews;
+
+    @BindView(R.id.tv_moveCounter)
+    TextView tvMoveCounter;
 
     @BindString(R.string.KEY_AIRPORT)
     String KEY_AIRPORT;
@@ -57,8 +64,6 @@ public class CounterFragment extends Fragment implements LoaderManager.LoaderCal
     @BindString(R.string.KEY_AIRPORT_ID)
     String KEY_AIRPORT_ID;
 
-    @BindView(R.id.tv_moveCounter)
-    TextView tvMoveCounter;
 
     private String airport = null;
     private String carrier = null;
@@ -75,6 +80,9 @@ public class CounterFragment extends Fragment implements LoaderManager.LoaderCal
         // Required empty public constructor
     }
 
+    boolean firstTime = true;
+    List<CounterConstraintView> counterViews = null;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -82,8 +90,7 @@ public class CounterFragment extends Fragment implements LoaderManager.LoaderCal
         // Inflate the layout for this fragment
 
         View rootView = inflater.inflate(R.layout.fragment_blank, container, false);
-        mView = (CounterViews) rootView.findViewById(R.id.custom_view);
-        ButterKnife.bind(this,rootView);
+        ButterKnife.bind(this, rootView);
 
 
         this.airport = getActivity().getIntent().getStringExtra(KEY_AIRPORT);
@@ -92,16 +99,14 @@ public class CounterFragment extends Fragment implements LoaderManager.LoaderCal
 
 
         // Get the flight details asynchronously
-        carrier_id = getActivity().getIntent().getIntExtra(KEY_CARRIER_ID,0);
+        carrier_id = getActivity().getIntent().getIntExtra(KEY_CARRIER_ID, 0);
 
-        Log.d(TAG, "onCreateView:  " + Integer.toString(carrier_id) );
+        Log.d(TAG, "onCreateView:  " + Integer.toString(carrier_id));
 
         firebaseDatabase = FirebaseDatabase.getInstance();
         reference = firebaseDatabase.getReference(this.airport + "/" + this.carrier + "/carrier");
 
-        Cursor mCursor =getActivity().getContentResolver().query(DatabaseContract.CounterEntry.buildCounterUri(carrier_id),null,null,null,null);
-        mView.setData(mCursor);
-        getLoaderManager().restartLoader(COUNTER_LOADER_ID,null,CounterFragment.this);
+        getLoaderManager().restartLoader(COUNTER_LOADER_ID, null, CounterFragment.this);
 
         reference.addValueEventListener(new ValueEventListener() {
             @Override
@@ -115,7 +120,7 @@ public class CounterFragment extends Fragment implements LoaderManager.LoaderCal
                     ContentValues values = new ContentValues();
                     values.put(DatabaseContract.CounterEntry.COLUMN_CARRIER_KEY, carrier_id);
 
-                    values.put(DatabaseContract.CounterEntry.COLUMN_COUNTER_AVG_WAITING_TIME, counter.getThroughput()*counter.getCounterCount());
+                    values.put(DatabaseContract.CounterEntry.COLUMN_COUNTER_AVG_WAITING_TIME, counter.getThroughput() * counter.getCounterCount());
                     values.put(DatabaseContract.CounterEntry.COLUMN_COUNTER_NUMBER, counter.getCounterNumber());
                     values.put(DatabaseContract.CounterEntry.COLUMN_COUNTER_THROUGHPUT, counter.getThroughput());
                     values.put(DatabaseContract.CounterEntry.COLUMN_COUNTER_COUNT, counter.getCounterCount());
@@ -141,7 +146,7 @@ public class CounterFragment extends Fragment implements LoaderManager.LoaderCal
                     null,
                     null,
                     null,
-                    DatabaseContract.CounterEntry.COLUMN_COUNTER_AVG_WAITING_TIME + " ASC"
+                    DatabaseContract.CounterEntry.COLUMN_COUNTER_NUMBER+ " ASC"
             );
         }
         return null;
@@ -150,21 +155,77 @@ public class CounterFragment extends Fragment implements LoaderManager.LoaderCal
     @Override
     public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
         Log.d(TAG, "onLoadFinished: " + data.getCount());
-        mView.setData(data);
 
-        data.moveToFirst();
-        String mini = data.getString(data.getColumnIndex(DatabaseContract.CounterEntry.COLUMN_COUNTER_AVG_WAITING_TIME));
-        String counter = data.getString(data.getColumnIndex(DatabaseContract.CounterEntry.COLUMN_COUNTER_NUMBER));
-        while(data.moveToNext()){
-            String currentMini = data.getString(data.getColumnIndex(DatabaseContract.CounterEntry.COLUMN_COUNTER_AVG_WAITING_TIME));
-            String counterMini = data.getString(data.getColumnIndex(DatabaseContract.CounterEntry.COLUMN_COUNTER_NUMBER));
+        if ( data.getCount() > 0 ){
 
-            if (Float.parseFloat(currentMini) < Float.parseFloat(mini))
-                counter = counterMini;
+            int minCounterNumber = 0;
+            float minWt = Integer.MAX_VALUE;
+
+
+            if ( firstTime ) {
+                firstTime = false;
+                data.moveToFirst();
+
+                counterViews = new ArrayList<>();
+
+                do {
+
+                    int counterCount = data.getInt(data.getColumnIndex(DatabaseContract.CounterEntry.COLUMN_COUNTER_COUNT));
+                    float throughput = data.getFloat(data.getColumnIndex(DatabaseContract.CounterEntry.COLUMN_COUNTER_THROUGHPUT));
+
+                    CounterConstraintView counterConstraintView = new CounterConstraintView(getActivity());
+                    counterConstraintView.setCount(counterCount);
+                    counterConstraintView.setAverageWaitingTime(throughput);
+
+                    float avgWt = throughput*counterCount;
+                    if ( avgWt < minWt ) {
+                        minWt = avgWt;
+                        minCounterNumber = data.getInt(data.getColumnIndex(DatabaseContract.CounterEntry.COLUMN_COUNTER_NUMBER));
+                    }
+
+                    // add the views
+                    LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(0, LinearLayout.LayoutParams.MATCH_PARENT, 1);
+                    llCounterViews.addView(counterConstraintView, layoutParams);
+                    counterViews.add(counterConstraintView);
+
+                } while (data.moveToNext());
+            }
+
+            else  {
+
+                int idx = 0;
+                data.moveToFirst();
+                do {
+                    CounterConstraintView counterConstraintView = counterViews.get(idx);
+                    int counterCount = data.getInt(data.getColumnIndex(DatabaseContract.CounterEntry.COLUMN_COUNTER_COUNT));
+                    int prevCounterCount = counterConstraintView.getCount();
+
+                    if ( prevCounterCount > counterCount ) {
+                        counterConstraintView.removeElements(prevCounterCount - counterCount);
+                    }
+
+                    else if ( prevCounterCount < counterCount ){
+                        counterConstraintView.addElements(counterCount - prevCounterCount);
+                    }
+
+                    float throughput = data.getFloat(data.getColumnIndex(DatabaseContract.CounterEntry.COLUMN_COUNTER_THROUGHPUT));
+                    counterConstraintView.setAverageWaitingTime(throughput);
+
+                    float avgWt = throughput*counterCount;
+                    if ( avgWt < minWt ) {
+                        minWt = avgWt;
+                        minCounterNumber = data.getInt(data.getColumnIndex(DatabaseContract.CounterEntry.COLUMN_COUNTER_NUMBER));
+                    }
+
+                    idx++;
+
+                } while (data.moveToNext());
+            }
+
+            tvMoveCounter.setText(String.format("%d", minCounterNumber));
         }
 
-        tvMoveCounter.setText(counter);
-
+        return;
     }
 
     @Override
